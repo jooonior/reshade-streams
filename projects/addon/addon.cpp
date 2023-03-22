7779ec28908a737ffaaee30dc84009d6de05df47
junior <>
2023-03-22 21:35:59 +0100

    Create solution and project

---
+++ projects/addon/addon.cpp
@@ -0,0 +1,20 @@
+#include <reshade.hpp>
+
+extern "C" __declspec(dllexport) const char *NAME = "ReShade Recorder";
+extern "C" __declspec(dllexport) const char *DESCRIPTION = "";
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD fdwReason, LPVOID)
+{
+	switch (fdwReason)
+	{
+	case DLL_PROCESS_ATTACH:
+		if (!reshade::register_addon(hModule))
+			return FALSE;
+		break;
+	case DLL_PROCESS_DETACH:
+		reshade::unregister_addon(hModule);
+		break;
+	}
+
+	return TRUE;
+}
