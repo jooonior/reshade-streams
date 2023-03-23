82a5ecd1a494c5b052e083bf55b80b3132cb81fb
junior <>
2023-03-23 23:41:33 +0100

    addon: Add std::format wrapper around reshade::log_message

--- projects/addon/stdafx.hpp
+++ projects/addon/stdafx.hpp
@@ -3,3 +3,16 @@
 #define ImTextureID ImU64  // Change ImGui texture ID type to that of a 'reshade::api::resource_view' handle.
 #include <imgui.h>
 #include <reshade.hpp>
+
+#include <format>
+#include <string>
+
+/// <summary>
+/// <see cref="std::format"/> wrapper around <see cref="reshade::log_message"/> for logging formatted messages.
+/// </summary>
+template<class... Args>
+inline void log_message(reshade::log_level level, std::format_string<Args...> fmt, Args&&... args)
+{
+	std::string message = std::format(fmt, std::forward<Args>(args)...);
+	reshade::log_message(level, message.c_str());
+}
