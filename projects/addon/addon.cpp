1c369481831288d85ed2e5adad55c5c26cceda94
junior <>
2023-03-23 20:46:12 +0100

    addon: Add effect runtime private data

--- projects/addon/addon.cpp
+++ projects/addon/addon.cpp
@@ -1,4 +1,15 @@
 #include "stdafx.hpp"
+#include "addon.hpp"
+
+static void on_init_effect_runtime(reshade::api::effect_runtime *runtime)
+{
+	runtime_data &data = runtime->create_private_data<runtime_data>();
+}
+
+static void on_destroy_effect_runtime(reshade::api::effect_runtime *runtime)
+{
+	runtime->destroy_private_data<runtime_data>();
+}
 
 extern "C" __declspec(dllexport) const char *NAME = "ReShade Recorder";
 extern "C" __declspec(dllexport) const char *DESCRIPTION = "";
@@ -10,6 +21,8 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD fdwReason, LPVOID)
 	case DLL_PROCESS_ATTACH:
 		if (!reshade::register_addon(hModule))
 			return FALSE;
+		reshade::register_event<reshade::addon_event::init_effect_runtime>(on_init_effect_runtime);
+		reshade::register_event<reshade::addon_event::destroy_effect_runtime>(on_destroy_effect_runtime);
 		break;
 	case DLL_PROCESS_DETACH:
 		reshade::unregister_addon(hModule);
---
+++ projects/addon/addon.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <reshade.hpp>
+
+struct __declspec(uuid("d1ffba56-33cc-45a5-a53b-8e8723cf0143")) runtime_data
+{
+};
