14f67d0e5417cdcd85f63f384adf3ff44d841454
junior <>
2023-03-24 15:06:28 +0100

    addon: #define NOMINMAX

--- projects/addon/stdafx.hpp
+++ projects/addon/stdafx.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#define NOMINMAX
+
 #define ImTextureID ImU64  // Change ImGui texture ID type to that of a 'reshade::api::resource_view' handle.
 #include <imgui.h>
 #include <reshade.hpp>
